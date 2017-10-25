958aa98e8b27b01d3ded277a15638d91c6eae5e5
garfield <shaudai0402@hotmail.com>
2017-10-25 17:38:56 +0800

    use argument to run different code

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+
+if (command === 'add') {
+	console.log('Adding new note');
+} else if (command === 'list') {
+	console.log('Listing all notes');
+} else {
+	console.log('Command not recognized');
+}
