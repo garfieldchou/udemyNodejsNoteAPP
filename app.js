58f7f849c47bf8fa4007165c93bcd495d8f4460e
garfield <shaudai0402@hotmail.com>
2017-10-25 15:10:08 +0800

    use isString() method in lodash module to check the argument is a string or not

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
+
+// console.log('Result:', notes.add(9, -2));
 
 // var user = os.userInfo();
 
