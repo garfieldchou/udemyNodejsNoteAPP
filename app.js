48de018c7731f839c532808b574e53ebaacf0389
garfield <shaudai0402@hotmail.com>
2017-10-25 11:36:13 +0800

    say hello to the user with user.username

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const os = require('os');
 
 var user = os.userInfo();
-console.log(user);
-//fs.appendFileSync('greetings.txt', 'Hello world!');
+
+fs.appendFileSync('greetings.txt', 'Hello ' + user.username + '!');
