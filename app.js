bf92f2221c3e0ba3cc5f91381874ea2b19adb889
garfield <shaudai0402@hotmail.com>
2017-10-25 10:17:27 +0800

    use appendFileSync function to avoid callback function

--- app.js
+++ app.js
@@ -2,8 +2,4 @@ console.log('Starting app.');
 
 const fs = require('fs');
 
-fs.appendFile('greetings.txt', 'Hello world!', function(err) {
-	if (err) {
-		console.log('Unable to write to file');
-	}
-});
+fs.appendFileSync('greetings.txt', 'Hello world!');
