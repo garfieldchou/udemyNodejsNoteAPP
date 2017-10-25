120c30ce4a03ad313515440479e4b18d3cf3aaf5
garfield <shaudai0402@hotmail.com>
2017-10-25 15:42:33 +0800

    use _.uniq() command to remove duplicate elements in an array

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log(_.isString(true));
-console.log(_.isString('Andrew'));
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+var filteredArray = _.uniq(['Andrew', 1, 'Andrew', 1, 2, 3, 4]);
+console.log(filteredArray);
 
 // console.log('Result:', notes.add(9, -2));
 
