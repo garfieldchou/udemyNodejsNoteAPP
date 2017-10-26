ff33546b60ff0f6970b3bf33857ad41b18f3cfb5
garfield <shaudai0402@hotmail.com>
2017-10-26 09:10:50 +0800

    install npm module- yargs to make parsing arguments easier
    compare process.argv[2] with yargs.argv to learn the differences

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+var argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command: ', command);
+console.log('Process', process.argv);
+console.log('Yargs', argv);
 
 if (command === 'add') {
 	console.log('Adding new note');
