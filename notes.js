744d8ee407e800adaa2d6d06b4415ade141f7f4b
garfield <shaudai0402@hotmail.com>
2017-10-27 09:02:36 +0800

    write a note to the json file if there's no note with the same title in that file

--- notes.js
+++ notes.js
@@ -18,6 +18,10 @@ var addNote = (title, body) => {
 
 	var duplicateNotes = notes.filter((note) => note.title === title);
 
+	if (duplicateNotes.length === 0) {
+		notes.push(note);
+		fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+	}
 };
 
 var getAll = () => {
